adeefc61020872ac3f8bf6ae57270947f7782f66
Palash3101 <mishrapalash2005@gmail.com>
2025-07-01 18:36:08 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-700 text-gray-800">
+      Hello World
+    </div>
+  )
+}
+
+export default App
