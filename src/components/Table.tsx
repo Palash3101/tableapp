4d766a2f7519e176961ee618da3db762db877209
Palash3101 <mishrapalash2005@gmail.com>
2025-07-04 13:38:06 +0530

    Implemented basic table
    Implemented a basic table, and added custom table column type

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,97 @@
+import { useState } from 'react';
+import { useTable} from 'react-table';
+
+import tableData from '../data/AllOrders';
+
+type RowData = Record<string, any>;
+
+type CustomColumn = {
+  Header: string;
+  accessor: string;
+  colour: string;
+  align: 'left' | 'right' | 'center';
+  width: number;
+};
+
 
 function Table() {
+
+  const [columns, setColumns] = useState<CustomColumn[]>(tableData.columns as CustomColumn[]);
+
+    const [data, setData]= useState<RowData[]>(tableData.data);
+
+
+
+    const {
+    getTableProps,
+    getTableBodyProps,
+    } = useTable({ columns, data });
+
+
+
+
+
   return (
-    <div>
-      Table
+    <div className='w-full h-full overflow-auto'>
+      <table {...getTableProps()} className='border-collapse'>
+        <thead>
+          <tr>
+          {
+            columns.map((column: CustomColumn, index:number) => (
+              <th 
+                key={index}
+                className={'text-[16px] pr-[4px] pl-[8px] border-y border-r border-[#f6f6f6] text-[#757575] font-[600] h-[32px]'}
+                style={{ width:`${column.width}px`, backgroundColor: `${column.colour}`}}
+              >
+              
+                {column.Header}
+              </th>
+              // console.log(column.Header);
+            ))
+          }
+          </tr>
+        </thead>
+
+        <tbody {...getTableBodyProps()} className=''>
+          {
+            data.map((row: RowData, rowIndex:number) => (
+              <tr 
+                key={rowIndex}
+              >
+                {
+                  columns.map((column:CustomColumn, columnIndex:number)=>(
+                    <td
+                      key={columnIndex}
+                      className={`border-b-[1px] border-r-[1px] border-[#f6f6f6] 
+                                ${column.accessor === "index" ? "text-[#757575]" : ""} 
+                                `}
+                    >
+                    { column.accessor === "index" ?
+                      <input 
+                        className={'table-column'}
+                        style={{ width:`${column.width}px`, textAlign: `${column.align}`}}
+                        value={row[column.accessor]}
+                        type="text"
+                        disabled
+                      />
+                      :
+                      <input 
+                        className={`table-column overflow-hidden truncate`}
+                        style={{ width:`${column.width}px`, textAlign: `${column.align}`}}
+                        value={row[column.accessor]}
+                        type="text"
+                        readOnly
+                      />
+                      
+                    }
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
---
+++ src/data/AllOrders.ts
@@ -0,0 +1,83 @@
+const tableData = {
+  columns: [
+      { Header: "#", accessor: "index", colour: "#EEEEEE", align: "center", width: 32 },
+      { Header: "Job Request", accessor: "job_request" , colour: "#EEEEEE", align: "left", width: 256 },
+      { Header: "Submitted", accessor: "submitted" , colour: "#EEEEEE", align: "right", width: 124 },
+      { Header: "Status", accessor: "status" , colour: "#EEEEEE", align: "center", width: 124 },
+      { Header: "Submitter", accessor: "submitter" , colour: "#EEEEEE", align: "left", width: 124 },
+
+      { Header: "URL", accessor: "url" , colour: "#EEEEEE", align: "left", width: 124 },
+
+      { Header: "Assigned To", accessor: "assigned_to" , colour: "#E8F0E9", align: "left", width: 124 },
+
+      { Header: "Priority", accessor: "priority", colour: "#EAE3FC", align: "center", width: 125 },
+      { Header: "Due Date", accessor: "due_date", colour: "#EAE3FC", align: "right", width: 125 },
+
+      { Header: "Est. Value", accessor: "est_value" , colour: "#FFE9E0", align: "right", width: 125 },
+    ],
+
+  data: [
+  {
+    index: 1,
+    job_request: "Launch social media campaign for product",
+    submitted: "15-11-2024",
+    status: "In-process",
+    submitter: "Aisha Patel",
+    url: "www.aishapatel.com",
+    assigned_to: "Sophie Choudhury",
+    priority: "Medium",
+    due_date: "20-11-2024",
+    est_value: "6,200,000 ₹"
+  },
+  {
+    index: 2,
+    job_request: "Update press kit for company redesign",
+    submitted: "28-10-2024",
+    status: "Need to start",
+    submitter: "Irfan Khan",
+    url: "www.irfankhanpress.com",
+    assigned_to: "Tejas Pandey",
+    priority: "High",
+    due_date: "30-10-2024",
+    est_value: "3,500,000 ₹"
+  },
+  {
+    index: 3,
+    job_request: "Finalize user testing feedback for app",
+    submitted: "05-12-2024",
+    status: "In-process",
+    submitter: "Mark Johnson",
+    url: "www.markjohnsondev.com",
+    assigned_to: "Rachel Lee",
+    priority: "Medium",
+    due_date: "10-12-2024",
+    est_value: "4,750,000 ₹"
+  },
+  {
+    index: 4,
+    job_request: "Design new features for the website",
+    submitted: "10-01-2025",
+    status: "Complete",
+    submitter: "Emily Green",
+    url: "www.emilygreenui.com",
+    assigned_to: "Tom Wright",
+    priority: "Low",
+    due_date: "15-01-2025",
+    est_value: "5,900,000 ₹"
+  },
+  {
+    index: 5,
+    job_request: "Prepare financial report for Q4",
+    submitted: "25-01-2025",
+    status: "Blocked",
+    submitter: "Jessica Brown",
+    url: "www.jessicabrownfinance.com",
+    assigned_to: "Kevin Smith",
+    priority: "Low",
+    due_date: "30-01-2025",
+    est_value: "2,800,000 ₹"
+  }
+]
+};
+
+export default tableData;
